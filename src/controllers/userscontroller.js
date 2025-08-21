4ff46822cbe1a1d682c6ed61251977d1463a1790
liateg <liyategared7@gmail.com>
2025-08-21 18:56:16 +0300

    registration handeled

--- src/controllers/userscontroller.js
+++ src/controllers/userscontroller.js
@@ -1,1 +1,37 @@
-// rigister,login,get user,edit profile,
+// rigister,login,get user,edit profile,delete user
+//   name         String
+//   username     String    @unique
+//   email        String    @unique
+//   passwordHash String
+
+
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+
+const prisma = new PrismaClient();
+const saltRounds = 10;
+export const createUser = async (req, res) => {
+  try {
+    const { name, email,password} = req.body;
+    bcrypt.hash(password,saltRounds,async(err,passwordHash)=>{
+      if(err){
+        console.error("Errore hasing",err.stack)
+      }else{
+  const user = await prisma.user.create({
+      data: { name, email, passwordHash },
+    });
+    res.json(user);
+      }
+    })
+  
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const getUsers = async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.json(users);
+};
+
+// ... (other functions same pattern)
