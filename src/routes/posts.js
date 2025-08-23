aa170adcafebf0167d8ea40ce07a03e23ef46ad7
liateg <liyategared7@gmail.com>
2025-08-23 23:02:45 +0300

    CRUD done

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -1,10 +1,11 @@
 import express from "express"
-import {creatPost,getPost,getPosts,updatePost} from "../controllers/postcontroller.js"
+import {creatPost,getPost,getPosts,updatePost,deletPost} from "../controllers/postcontroller.js"
 
 const router=express.Router()
 
 router.post("/",creatPost)
 router.get("/",getPosts)
 router.get("/:id",getPost)
-router.post("/:id",updatePost)
+router.put("/:id",updatePost)
+router.delete("/:id",deletPost)
 export default router
