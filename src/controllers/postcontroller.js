d1ddb2e835436a0006f4baab89bada63dee2acf3
liateg <liyategared7@gmail.com>
2025-08-25 22:55:21 +0300

    ai summery done

--- src/controllers/postcontroller.js
+++ src/controllers/postcontroller.js
@@ -1,6 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
-import generateSummary from "./aicontentscontroller"
+import {generateSummary} from "./aicontentscontroller.js"
 
 export const creatPost=async (req,res)=>{
     
